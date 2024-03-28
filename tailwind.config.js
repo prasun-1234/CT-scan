5d187f7efd0e950a6f8bdffb3758f2aa86742147
prasun-1234 <prasun@quadranttech.co>
2024-03-28 18:10:29 +0530

    new two section added related to XPERTMRI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./*.{html,js}"],
+  // original CT configuration
+  // theme: {
+  //   container: {
+  //     screens: {
+  //       xs: "425px",
+  //       sm: "640px",
+  //       md: "767px",
+  //       lg: "1024px",
+  //       xl: "1240px",
+  //       "2xl": "1536px",
+  //     },
+  //   },
+  //   extend: {},
+  // },
   theme: {
     container: {
-      // you can configure the container to be centered
       center: true,
-      // or have default horizontal padding
-      padding: '1.2rem',
-      // default breakpoints but with 40px removed
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
       screens: {
-        xs: '425px',
-        sm: '640px',
-        md: '767px',
-        lg: '1024px',
-        xl: '1240px',
+        DEFAULT: '100%',
+        xl: '1280px',
         '2xl': '1536px',
+        '3xl': '1920px'
+      },
+      extend: {
+        padding: '80px',
       },
     },
-    extend: {},
   },
   plugins: [],
-}
-
+};
